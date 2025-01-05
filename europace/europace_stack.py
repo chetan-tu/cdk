799bffa7368d1e96000f9dd892adb36ae96ef72d
Chetan <chetanparashar.tu@gmail.com>
2025-01-05 21:40:37 +0100

    Add VPC with isolated private subnets for the private web service

--- europace/europace_stack.py
+++ europace/europace_stack.py
@@ -1,19 +1,27 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_ec2 as ec2,
+    aws_iam as iam,
 )
 from constructs import Construct
+
 
 class EuropaceStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "EuropaceQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # defining the VPC
+        private_web_service_vpc = ec2.Vpc(
+            self, "PrivateWebServiceVPC",
+            ip_addresses=ec2.IpAddresses.cidr("10.0.0.0/16"),
+            max_azs=2,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="PrivateWebServiceSubnet",
+                    subnet_type=ec2.SubnetType.PRIVATE_ISOLATED,
+                    cidr_mask=24
+                )
+            ]
+        )
